e926017e1e714365d16d864b48dea0dfb52ef6db
Doubz <747354662@qq.com>
2023-07-11 23:27:16 +0800

    feat:init Pages

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,97 @@
+export default {
+  title: 'Dubzeey', // 博客的标题
+  description: 'Dubzeey的个人博客', // 博客的介绍
+  base: '/blog/', // 如果想用 https://Doubz.github.io/blog/ 访问，那么这句话必填
+  themeConfig: {
+      logo: "/images/logo.png", // 页面上显示的logo
+      nav: [ // 页面右上角的导航
+          { text: "vue", link: "/articles/js-core/this指向问题" },
+          { text: "uniapp", link: "/articles/js-core/this指向问题" },
+          {
+              text: '博客文档',
+              items: [ // 可以配置成下拉
+                  { text: 'JavaScript 核心系列', link: '/articles/js-core/this指向问题' },
+                  { text: 'Vue 三方组件库', link: '/articles/js-core/this指向问题' },
+                  { text: '其他', link: '/articles/js-core/this指向问题' },
+              ]
+          }
+      ],
+      sidebar: { // 侧边栏，可以分组
+          "/articles/vue/": [
+              {
+                  text: "基础",
+                  items: [
+                  ],
+              },
+              {
+                  text: "代码段",
+                  items: [
+                      {
+                          text: "上传素材到COS",
+                          link: "/articles/vue/上传素材到COS",
+                      },
+                      {
+                          text: "文件下载",
+                          link: "/articles/vue/文件下载",
+                      },
+                  ],
+              },
+          ],
+          "/articles/uniapp/": [
+              {
+                  text: "基础",
+                  items: [
+                  ],
+              },
+              {
+                  text: "代码段",
+                  items: [
+                      {
+                          text: "一键登录",
+                          link: "/articles/js-core/this指向问题",
+                      }
+                  ],
+              },
+          ],
+          "/articles/javaScript-core/": [
+              {
+                  text: "基础",
+                  items: [
+                  {
+                      text: "1. 构造函数、原型、原型链",
+                      link: "/articles/javaScript-core/构造函数、原型、原型链",
+                  },
+                  {
+                      text: "2. 执行上下文和执行上下文栈",
+                      link: "/articles/javaScript-core/执行上下文和执行上下文栈",
+                  },
+                  {
+                      text: "3. this的指向",
+                      link: "/articles/javaScript-core/this的指向",
+                  },
+                  ],
+              },
+              {
+                  text: "进阶",
+                  items: [
+                  {
+                      text: "xx",
+                      link: "/xx",
+                  },
+                  ],
+              },
+          ],
+          "/articles/libs/": [
+              {
+                  items: [
+                  {
+                      text: "1. VForm3低代码初体验",
+                      link: "/articles/libs/VForm3低代码初体验",
+                  },
+                  ],
+              }
+          ],
+      },
+      socialLinks: [{ icon: "github", link: "https://github.com/Doubz" }], // 可以连接到 github
+  }
+}
---
+++ docs/index.md
@@ -0,0 +1,37 @@
+---
+layout: home
+
+title: Dubzeey
+titleTemplate: 一个 coder 的博客
+
+hero:
+  name: Dubzeey
+#   text: 一个 coder 的博客
+  tagline: 有时候想到不知道怎么说，就记录下来；遇到了不想错过，也记录下来；我脑子装不下东西了
+  image:
+    src: /images/logo.png
+    alt: Dubzeey
+  actions:
+    - theme: brand
+      text: 开始使用
+      link: /ariticles/js-core/this指向问题
+    - theme: alt
+      text: 在 GitHub 上查看
+      link: https://github.com/Doubz
+features:
+  - icon: 🌞
+    title: javaScript 核心系列
+    details: 基础知识积累回顾
+    link: /ariticles/js-core/this指向问题
+    linkText: 查看更多
+  - icon: ⚡️
+    title: Vue 三方组件库
+    details: 一些使用过的很好用的三方库
+    link: /ariticles/js-core/this指向问题
+    linkText: 查看更多
+  - icon: 🛠️
+    title: 其他
+    details: 一些积累，操作，边缘事情
+    link: "/ariticles/js-core/this指向问题"
+    linkText: 查看更多
+---
